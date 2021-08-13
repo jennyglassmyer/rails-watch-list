c63f8c679fcd2b779c9278862cdddacb54b007f4
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-08-13 14:03:31 +0200

    routes, controller, views for bookmark class

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(bookmark_params[:movie_id])
+    @bookmark = Bookmark.new(
+      comment: bookmark_params[:comment],
+      movie: @movie,
+      list: @list
+      )
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
